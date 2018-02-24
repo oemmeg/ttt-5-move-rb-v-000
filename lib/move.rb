5d97db3878f5a149a5421fdc0ca7a4fbd65c161b
Emme Porter DeWitt <empdewitt@gmail.com>
2018-02-24 03:25:06 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,7 +7,7 @@ def display_board(board)
 end
 
 def input_to_index(user_input)
-  user_input = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  user_input = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 end
 
 def to_i(converted_input)
