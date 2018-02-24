ec399a9b02823b0f7b70134f2019ad4f9b5d2bca
Emme Porter DeWitt <empdewitt@gmail.com>
2018-02-24 04:13:19 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -9,6 +9,6 @@ end
 def input_to_index(user_input)
   user_input = ["1", "2", "3", "4", "5", "6", "7", "8", "9"] 
   converted_input = input_to_index(user_input)
-  user_input[0] = Integer(0)
+  user_input[0] = "0"
   puts converted_input
 end
